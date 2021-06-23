75fde4f7ac8c2d2a7447e33dc64f2067019bb1cd
Leon <56673713+ledi64@users.noreply.github.com>
2021-06-23 18:28:47 +0200

    Add files via upload

--- RampsFinal.ino
+++ RampsFinal.ino
@@ -2,7 +2,7 @@
  * RAMPS 1.4 Shield Arduino for Farming Bot
  * author:      Leon Diel
  * date:        16/03/2021
- * last update: 08/06/2021
+ * last update: 23/06/2021
  * 
  * This code is a test for the first dimensioning of the Farming Bot.
  * The USB-Serialcommunication between Pi and Uno is not implemented, yet.
@@ -14,12 +14,13 @@
 
 #include <string.h>
 #include <Thread.h>
+#include <Servo.h>
 
 //==================
 // Achsen in Zahlen
-//    x   0
-//    y   1
-//    z   2
+//    x   1
+//    y   2
+//    z   3
 //==================
 
 //=================
@@ -64,6 +65,12 @@
 #define FLOOR_2     23
 #define FLOOR_3     25
 
+// Servo-Pins
+#define SERVO_ROTATE   11
+#define SERVO_LEFT     6
+#define SERVO_RIGHT    5
+#define SERVO_GRAB     4
+
 //========
 // Arrays
 //========
@@ -92,8 +99,9 @@ int limit_max_z = 0;
 // Define other constants
 const int wait_ms = 250;
 const int delaymicro = 250;
+const int speed_x = 550;
 const int steps = 5000;
-const int speed_homing = 400;
+const int speed_homing = 450;
 
 int fl0, fl1, fl2, fl3 = 0;
 int floor_array[] = {0, 0, 0, 0};
@@ -109,7 +117,8 @@ int dif = 0;
 int curr_position_x[] = {current_x_p, new_x_p};
 int curr_position_z[] = {current_z_p, new_z_p};
 
-// Step Counter Z
+// Step Counter
+int count_steps_x = 0;
 int count_steps_z = 0;
 
 // Kommunikationsvariable
@@ -165,205 +174,258 @@ void setup() {
 
   LimitThread.onRun(limit);
   LimitThread.setInterval(100);
-  ComThread.onRun(communicate);
-  ComThread.setInterval(100);
+  //ComThread.onRun(communicate);
+  //ComThread.setInterval(100);
   MovingThread.onRun(movebot);
   MovingThread.setInterval(100);
 
-  home_x();
-  home_z();
+  Servo Srotate;
+  Servo Sleft;
+  Servo Sright;
+  Servo Sgrab;
+
+  Srotate.attach(SERVO_ROTATE);
+  Sleft.attach(SERVO_LEFT);
+  Sright.attach(SERVO_RIGHT);
+  Sgrab.attach(SERVO_GRAB);
+
+  //home_x();
+  //home_z();
+  //home_gripper();
+  
 }
 
 void loop()
 {
+  
   //ComThread.run();
   //LimitThread.run();
   //MovingThread.run();
-
+  
   while (boool == true)
   {
-    movebot();
+    //movebot();  
 
-//    Serial.println("Void Movebot...");
-//    //home_z();
-//    Serial.println("Moving Z to 2000");
-//    moveto(2, 2000);
-//    delay(1000);
-//    Serial.println("Moving Z to 3000");
-//    moveto(2, 3000);
-//    delay(1000);
-//    Serial.println("Moving Z to 1000");
-//    moveto(2, 1000);
-//    delay(1000);
-//    Serial.println("Moving Z to 4000");
-//    moveto(2, 4000);
-//    delay(1000);
-//    Serial.println("Homing Z");
-//    home_z();
-//
-//    
+    
+
+    
     boool = false;
   }
-  //home_z();
 }
 
 void movebot()
 {
   Serial.println("Void Movebot...");
-  //home_z();
-  Serial.println("Moving Z to 2000");
-  moveto(2, 2000);
+  delay(500);
+  Serial.println("Z 3000");
+  Serial.println("X 1500");
+  moveto(1500, 0, 3000);
   delay(1000);
-  Serial.println("Moving Z to 3000");
-  moveto(2, 3000);
+  Serial.println("Z 5000");
+  Serial.println("X 2000");
+  moveto(2000, 0, 5000);
   delay(1000);
-  Serial.println("Moving Z to 1000");
-  moveto(2, 1000);
+  Serial.println("Z 1500");
+  Serial.println("X 1000");
+  moveto(1000, 0, 1500);
   delay(1000);
-  Serial.println("Moving Z to 4000");
-  moveto(2, 4000);
+  Serial.println("Z 4000");
+  Serial.println("X 1250");
+  moveto(1250, 0, 4000);
   delay(1000);
-  Serial.println("Moving Z to 5000");
-  moveto(2, 5000);
+  Serial.println("Z 6000");
+  Serial.println("X 500");
+  moveto(500, 0, 6000);
   delay(1000);
-  Serial.println("Moving Z to 1500");
-  moveto(2, 1500);
+  Serial.println("Z 5500");
+  Serial.println("X 2000");
+  moveto(2000, 0, 5500);
   delay(1000);
-  Serial.println("Moving Z to 5250");
-  moveto(2, 5250);
-  delay(1000);
-  
+
+  home_x();
   home_z();
+  
   
 }
 
-void moveto(int axis, int storey)
+void moveto(int x, int y, int z)
 {
-  if (storey < 7587)
+  movezto(z);
+  delay(200);
+  movexto(x);
+}
+
+
+void movexto(int storey)
+{ 
+  if (storey > 3000)
   {
-    Serial.println("Error 01: Wron axis-number.");
+    Serial.println("Error 01: Number of Steps too high \nor the position not available, yet.");
+    return;
+  }
+  Serial.print("Moving X Axis to");
+  Serial.print(storey);
+  Serial.println("...");
+        
+  new_x_p = storey;
+  curr_position_x[1] = new_x_p;
+  Serial.println("CurrPos[1]:");
+  Serial.println(curr_position_x[1]);
+  current_x_p = curr_position_x[0];
+
+  if ((curr_position_x[1]) > (curr_position_x[0]))
+  {
+    dif = (curr_position_x[1] - curr_position_x[0]);
+    digitalWrite(X_DIR, LOW);
+    count_steps_x = 0;
+    
+    for (int i = 0; i < dif; i++)
+    {
+      digitalWrite(X_STEP, HIGH);
+      delayMicroseconds(speed_x);
+
+      digitalWrite(X_STEP, LOW);
+      delayMicroseconds(speed_x);
+      
+      count_steps_x++;
+    }
+    curr_position_x[0] = curr_position_x[1];
+    Serial.println("Array 0: ");
+    Serial.println(curr_position_x[0]);
+    Serial.println("X Position wurde angefahren.");
+    Serial.print(count_steps_x);
+    Serial.println(" Anzahl Schritte notwendig.");
+  }
+  else if ((curr_position_x[1]) < (curr_position_x[0]))
+  {
+    dif = (curr_position_x[0] - curr_position_x[1]);
+    digitalWrite(X_DIR, HIGH);
+    count_steps_x = 0;
+    
+    for (int i = 0; i < dif; i++)
+    {
+      digitalWrite(X_STEP, HIGH);
+      delayMicroseconds(speed_x);
+
+      digitalWrite(X_STEP, LOW);
+      delayMicroseconds(speed_x);
+
+      count_steps_x++;
+    }
+    curr_position_x[0] = curr_position_x[1];
+    Serial.println("X Position wurde angefahren.");
+    Serial.print(count_steps_x);
+    Serial.println(" Anzahl Schritte notwendig.");
+  }
+  else if ((curr_position_x[1]) == (curr_position_x[0]))
+  {
+    Serial.println("X Position ist bereits angefahren.");
+  }
+  else
+  {
+    Serial.println("Error 02.1: Something failed the 'curr_position_x'-Array. Please check your intype.");
+    return;
+  }
+}
+
+void movezto(int storey)
+{
+  if (storey > 7587)
+  {
+    Serial.println("Error 01: Number of Steps too high \nor the position not available, yet.");
     return;
   }
   
-  switch(axis)
-  {
-    case 0:
-      Serial.println("X not available yet.");
-      break;
-
-    case 1:
-      Serial.println("Y not available yet.");
-      break;
-
-    case 2:
-      Serial.print("Moving Z Axis to");
-      Serial.print(storey);
-      Serial.println("...");
-            
-      new_z_p = storey;
-      curr_position_z[1] = new_z_p;
-      Serial.println("CurrPos[1]:");
-      Serial.println(curr_position_z[1]);
-      current_z_p = curr_position_z[0];
-
-      if ((curr_position_z[1]) > (curr_position_z[0]))
-      {
-        dif = (curr_position_z[1] - curr_position_z[0]);
-        digitalWrite(Z_DIR, LOW);
-        digitalWrite(Y_DIR, HIGH);
-        count_steps_z = 0;
+  Serial.print("Moving Z Axis to");
+  Serial.print(storey);
+  Serial.println("...");
         
-        for (int i = 0; i < dif; i++)
-        {
-          digitalWrite(Z_STEP, HIGH);
-          delayMicroseconds(delaymicro);
-    
-          digitalWrite(Z_STEP, LOW);
-          delayMicroseconds(delaymicro);
-    
-          digitalWrite(Y_STEP, HIGH);
-          delayMicroseconds(delaymicro);
-    
-          digitalWrite(Y_STEP, LOW);
-          delayMicroseconds(delaymicro);
-          count_steps_z++;
-        }
-        curr_position_z[0] = curr_position_z[1];
-        Serial.println("Array 0: ");
-        Serial.println(curr_position_z[0]);
-        Serial.println("Position wurde angefahren.");
-        Serial.print(count_steps_z);
-        Serial.println(" Anzahl Schritte notwendig.");
-      }
-      else if ((curr_position_z[1]) < (curr_position_z[0]))
-      {
-        dif = (curr_position_z[0] - curr_position_z[1]);
-        digitalWrite(Z_DIR, HIGH);
-        digitalWrite(Y_DIR, LOW);
-        count_steps_z = 0;
-        
-        for (int i = 0; i < dif; i++)
-        {
-          digitalWrite(Z_STEP, HIGH);
-          delayMicroseconds(delaymicro);
-    
-          digitalWrite(Z_STEP, LOW);
-          delayMicroseconds(delaymicro);
-    
-          digitalWrite(Y_STEP, HIGH);
-          delayMicroseconds(delaymicro);
-    
-          digitalWrite(Y_STEP, LOW);
-          delayMicroseconds(delaymicro);
-          count_steps_z++;
-        }
-        curr_position_z[0] = curr_position_z[1];
-        Serial.println("Position wurde angefahren.");
-        Serial.print(count_steps_z);
-        Serial.println(" Anzahl Schritte notwendig.");
-      }
-      else if ((curr_position_z[1]) == (curr_position_z[0]))
-      {
-        Serial.println("Position ist bereits angefahren.");
-      }
-      else
-      {
-        Serial.println("Error 02: Something failed the 'curr_position_z'-Array. Please check your intype.");
-        break;
-      }
-    default:
-      Serial.println("Error 03: Wron axis-number.");
-  }
-}
+  new_z_p = storey;
+  curr_position_z[1] = new_z_p;
+  Serial.println("CurrPos[1]:");
+  Serial.println(curr_position_z[1]);
+  current_z_p = curr_position_z[0];
 
-void communicate()
-{
-  if (Serial.available() > 0)
+  if ((curr_position_z[1]) > (curr_position_z[0]))
   {
-    received = Serial.read();
-
-    switch (received)
+    dif = (curr_position_z[1] - curr_position_z[0]);
+    digitalWrite(Z_DIR, LOW);
+    digitalWrite(Y_DIR, HIGH);
+    count_steps_z = 0;
+    
+    for (int i = 0; i < dif; i++)
     {
-      case '1':
-        Serial.println("You typed: 1");
-        home_z();
+      digitalWrite(Z_STEP, HIGH);
+      delayMicroseconds(delaymicro);
 
-      case '2':
-        Serial.println("You typed: 2");
-        Serial.println("'Home_X()' is not available, yet!");
+      digitalWrite(Z_STEP, LOW);
+      delayMicroseconds(delaymicro);
+
+      digitalWrite(Y_STEP, HIGH);
+      delayMicroseconds(delaymicro);
+
+      digitalWrite(Y_STEP, LOW);
+      delayMicroseconds(delaymicro);
+      count_steps_z++;
     }
-    Serial.flush();
-    received = ' ';  
+    curr_position_z[0] = curr_position_z[1];
+    Serial.println("Array 0: ");
+    Serial.println(curr_position_z[0]);
+    Serial.println("Z Position wurde angefahren.");
+    Serial.print(count_steps_z);
+    Serial.println(" Anzahl Schritte notwendig.");
+  }
+  else if ((curr_position_z[1]) < (curr_position_z[0]))
+  {
+    dif = (curr_position_z[0] - curr_position_z[1]);
+    digitalWrite(Z_DIR, HIGH);
+    digitalWrite(Y_DIR, LOW);
+    count_steps_z = 0;
+    
+    for (int i = 0; i < dif; i++)
+    {
+      digitalWrite(Z_STEP, HIGH);
+      delayMicroseconds(delaymicro);
+
+      digitalWrite(Z_STEP, LOW);
+      delayMicroseconds(delaymicro);
+
+      digitalWrite(Y_STEP, HIGH);
+      delayMicroseconds(delaymicro);
+
+      digitalWrite(Y_STEP, LOW);
+      delayMicroseconds(delaymicro);
+      count_steps_z++;
+    }
+    curr_position_z[0] = curr_position_z[1];
+    Serial.println("Z Position wurde angefahren.");
+    Serial.print(count_steps_z);
+    Serial.println(" Anzahl Schritte notwendig.");
+  }
+  else if ((curr_position_z[1]) == (curr_position_z[0]))
+  {
+    Serial.println("Z Position ist bereits angefahren.");
+  }
+  else
+  {
+    Serial.println("Error 02.3: Something failed the 'curr_position_z'-Array. Please check your intype.");
+    return;
   }
 }
+
+void grab_position()
+{
+  
+}
+
 
 void home_x()
 {
-  digitalWrite(X_DIR, LOW);
-  Serial.println("Homing Z...");
+  digitalWrite(X_DIR, HIGH);
+  Serial.println("Homing X...");
   
   limit_min_x = digitalRead(END_MIN_X);
-  while (limit_min_z == 1)
+  while (limit_min_x == 1)
   {
     limit_min_x = digitalRead(END_MIN_X);
 
@@ -411,6 +473,46 @@ void home_z()
   //digitalWrite(Y_ENABLE,1);
   //digitalWrite(Z_ENABLE,1);
   delay(500);
+}
+
+void home_rotate_servo()
+{
+  
+}
+
+void home_gripper()
+{
+  Serial.println("Homing Gripper...");
+  
+  home_rotate_servo();
+
+  Serial.println("Gripper homed.");
+}
+
+void xtest()
+{
+  digitalWrite(X_DIR, LOW);
+  
+  for (int i = 0; i < 1000; i++)
+  {
+
+    digitalWrite(X_STEP, HIGH);
+    delayMicroseconds(speed_homing);
+    
+    digitalWrite(X_STEP, LOW);
+    delayMicroseconds(speed_homing);
+  }
+  digitalWrite(X_DIR, HIGH);
+  
+  for (int i = 0; i < 1000; i++)
+  {
+
+    digitalWrite(X_STEP, HIGH);
+    delayMicroseconds(speed_homing);
+    
+    digitalWrite(X_STEP, LOW);
+    delayMicroseconds(speed_homing);
+  }
 }
 
 void limit()
